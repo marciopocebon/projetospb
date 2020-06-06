65cbe3f5766399e85c94f1e5f02a9d2dfcb4acc9
Alison Rodrigues <alison.aro@gmail.com>
2020-06-06 15:06:25 -0300

    Projeto Criado

---
+++ src/main/java/com/alison/projetospb/ProjetospbApplication.java
@@ -0,0 +1,13 @@
+package com.alison.projetospb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alison/projetospb/ProjetospbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alison.projetospb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
