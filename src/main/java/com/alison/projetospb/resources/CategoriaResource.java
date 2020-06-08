63f669326178b7e0a99298523f415fd4d7c9acaa
Alison Rodrigues <alison.aro@gmail.com>
2020-06-08 00:39:29 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/alison/projetospb/resources/CategoriaResource.java
+++ src/main/java/com/alison/projetospb/resources/CategoriaResource.java
@@ -3,26 +3,36 @@ package com.alison.projetospb.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alison.projetospb.domain.Categoria;
+import com.alison.projetospb.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(1, "Informatica");
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
+		/*Categoria c1 = new Categoria(1, "Informatica");
 		Categoria c2 = new Categoria(2, "Escritorio");
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(c1);
 		lista.add(c2);
 		
-		return lista;
+		return lista;*/
 	}
 	
 }
